0af4a4ae87c86a301d8205b0353f82c3e99019b0
Archie Hewitt <hewitt.archie@yandex.ru>
2022-12-02 19:49:21 +0300

    Fixed packages versions

--- run_classification_trn_val_tst_sa.py
+++ run_classification_trn_val_tst_sa.py
@@ -1,11 +1,19 @@
 import dotenv
 import hydra
 from omegaconf import DictConfig
+import pyrootutils
 
 # load environment variables from `.env` file if it exists
 # recursively searches for `.env` in all folders starting from work dir
 dotenv.load_dotenv(override=True)
 
+# project root setup
+# searches for root indicators in parent dirs, like ".git", "pyproject.toml", etc.
+# sets PROJECT_ROOT environment variable (used in `configs/paths/default.yaml`)
+# loads environment variables from ".env" if exists
+# adds root dir to the PYTHONPATH (so this file can be run from any place)
+# https://github.com/ashleve/pyrootutils
+root = pyrootutils.setup_root(__file__, dotenv=True, pythonpath=True)
 
 @hydra.main(config_path="configs/", config_name="main.yaml")
 def main(config: DictConfig):
